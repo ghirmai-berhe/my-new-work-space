2e990118de4b4ef35c8e47acf3a2464ed8db8a30
Ghirmai Berhe <estifji1@gmail.com>
2024-10-28 19:26:27 -0500

    remodelling-currency-conversion

---
+++ currency-conversion-service-v1/src/main/java/com/ghirmaiudemycourse/microservices/currencyconversionservicev1/service/CurrencyConversionService.java
@@ -0,0 +1,8 @@
+package com.ghirmaiudemycourse.microservices.currencyconversionservicev1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CurrencyConversionService {
+
+}
